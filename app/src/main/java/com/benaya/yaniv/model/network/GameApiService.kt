30b8af68e7d5d90d6a63a0328748e27aa6e7acde
= <shaul.chamoula@mail.huji.ac.il>
2021-01-04 18:56:31 +0200

    create @POST("games/") in interface GameApiService @ GameApiService.kt

--- app/src/main/java/com/benaya/yaniv/model/network/GameApiService.kt
+++ app/src/main/java/com/benaya/yaniv/model/network/GameApiService.kt
@@ -3,9 +3,13 @@ package com.benaya.yaniv.model.network
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface GameApiService {
     @GET("games/")
-    fun fetchGameStatus(@Path("id") id: Int, @Header("apikey") apikey: String): Call<GameResponse>
+    fun getGameStatus(@Path("id") id: Int, @Header("apikey") apikey: String): Call<GameResponse>
+
+    @POST("games/")
+    fun postGamesStatus(@Header("apikey") apikey: String): Call<GameResponse>
 }

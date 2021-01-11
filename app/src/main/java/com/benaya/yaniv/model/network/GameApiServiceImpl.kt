bc51e59f89617204c6b1f72fe46f3341b6ea3fe2
= <shaul.chamoula@mail.huji.ac.il>
2021-01-11 13:38:24 +0200

    remove last slash in baseUrl in GameApiServiceImpl.kt

--- app/src/main/java/com/benaya/yaniv/model/network/GameApiServiceImpl.kt
+++ app/src/main/java/com/benaya/yaniv/model/network/GameApiServiceImpl.kt
@@ -9,7 +9,7 @@ object GameApiServiceImpl {
     val service: GameApiService by lazy { getRetrofit().create(GameApiService::class.java)}
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://androidoss-yaniv.herokuapp.com/")
+            .baseUrl("https://androidoss-yaniv.herokuapp.com")
             .addConverterFactory(
                 Json { ignoreUnknownKeys = true}
                     .asConverterFactory(MediaType.get("application/json"))

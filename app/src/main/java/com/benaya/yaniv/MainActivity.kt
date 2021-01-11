2236be7821d2c8b7858b1ba209f94015b0d5a56d
= <shaul.chamoula@mail.huji.ac.il>
2021-01-11 13:37:44 +0200

    joinGame() loadGame() loadGamePeriodically() in MainActivity.kt

--- app/src/main/java/com/benaya/yaniv/MainActivity.kt
+++ app/src/main/java/com/benaya/yaniv/MainActivity.kt
@@ -2,7 +2,9 @@ package com.benaya.yaniv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.benaya.yaniv.Data.Card
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var playersAdapter: PlayersAdapter
     lateinit var cardsListView: RecyclerView
     lateinit var playersListView: RecyclerView
+    private val handler = Handler()
+
+    //TODO: Ask Orel what is the right way to initialize gameId
+    var gameId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,22 +37,40 @@ class MainActivity : AppCompatActivity() {
 
         cardsAdapter = CardsAdapter()
         cardsListView.adapter = cardsAdapter
-//        cardsListView.adapter?.notifyDataSetChanged()
 
         playersAdapter = PlayersAdapter()
         playersListView.adapter = playersAdapter
-//        playersListView.adapter?.notifyDataSetChanged()
 
-        loadGame()
+        joinGame()
+
     }
 
-    private fun loadGame() {
+    private fun joinGame() {
         val call = GameApiServiceImpl
             .service
-            .getGameStatus(14, "168a0b51-5459-42ea-a002-02d7e388340b")
+            .postGamesStatus("168a0b51-5459-42ea-a002-02d7e388340b")
 
+        Log.e(MainActivity::javaClass.name, "in joinGame")
         call.enqueue(GameCallback())
+    }
 
+    private fun loadGame(gameId: Int) {
+        val call = GameApiServiceImpl
+            .service
+            .getGameStatus(gameId, "168a0b51-5459-42ea-a002-02d7e388340b")
+
+        findViewById<TextView>(R.id.statusTV).text = "loadGame"
+        Log.e(MainActivity::javaClass.name, "in loadGame")
+        call.enqueue(GameCallback())
+    }
+
+    private fun loadGamePeriodically(gameId: Int?) {
+        gameId?.let {
+            handler.postDelayed({
+                loadGame(gameId)
+            }, 5000)
+        }
+        Log.e(MainActivity::javaClass.name, "in loadGamePeriodically")
         //setProgressVisibility(true)
     }
 
@@ -55,28 +79,40 @@ class MainActivity : AppCompatActivity() {
 
         override fun onResponse(call: Call<Game>, response: Response<Game>) {
             //TODO: update progressBar
-            statusTV.text = "fetching game"
+            Log.e(MainActivity::javaClass.name, "onResponse fetching game")
 
             if (response.isSuccessful) {
                 response.body()?.let { game ->
+                    if (gameId != game.id)
+                        gameId = game.id
                     game.players.find(this::isPlayerMe)?.let {
                         cardsAdapter.submitList(it.cards)
+
+                        val sum = sumCardsValues(it.cards)
+                        //https://kotlinlang.org/docs/reference/control-flow.html
+                        findViewById<Button>(R.id.yanivButton).isEnabled = sum <= 7
+
+                        //statusTV.text = "The sum of your cards' values is: $sum in game#: $gameId"
                     }
+
                     val opponents = game.players.filterNot(this::isPlayerMe)
                     playersAdapter.submitList(opponents)
                 }
-                statusTV.text = "game fetched"
+                Log.e(MainActivity::javaClass.name, "response is successful; game fetched.")
             } else {
                 statusTV.text =
                     "response.isSuccessful = ${response.isSuccessful}.  ${response.message()}"
             }
+
+            loadGamePeriodically(gameId)
         }
 
         private fun isPlayerMe(player: Player) = player.userId == 4
 
         override fun onFailure(call: Call<Game>, t: Throwable) {
-            statusTV.text = "onFailure\n"
+            //statusTV.text = "onFailure\n"
             Log.e(MainActivity::javaClass.name, "game request failed", t)
+            loadGamePeriodically(gameId)
         }
     }
 
